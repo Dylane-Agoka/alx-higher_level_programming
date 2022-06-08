1bc5fb587347fd60d563fdd137a640fd2f227789
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-06-08 23:39:44 +0100

    roman to in

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numeral = {'I': 1, 'V': 5, 'X': 10,
+                     'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    if type(roman_string) != str or type(roman_string) == None:
+        return 0
+    for roman in roman_string:
+        sum += roman_numeral[roman]
+    return sum
