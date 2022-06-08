91e57601f64f9b47c64a39951753926eb374b558
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-06-08 23:52:55 +0100

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from types import NoneType
-
-
 def roman_to_int(roman_string):
     roman_numeral = {'I': 1, 'V': 5, 'X': 10,
                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
-    if type(roman_string) != str or type(roman_string) is NoneType:
+    if type(roman_string) != str or type(roman_string) is None:
         return 0
     for roman in roman_string:
         sum += roman_numeral[roman]
